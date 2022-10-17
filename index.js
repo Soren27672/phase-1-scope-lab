d45a731415e455414d6fbb8482e8ce34f9566974
Maxwell Gottesman <maxgman16@yahoo.com>
2022-10-17 18:22:44 -0400

    commit tree f1048d7fe27aef40b9deea31a767724a11e7d316

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+var setBestCustomer = () => bestCustomer = 'not bob';
+
+var overwriteBestCustomer = () => bestCustomer = 'maybe bob';
+
+const leastFavoriteCustomer = 'bob lol';
+
+var changeLeastFavoriteCustomer = (name = 'loony toons') => leastFavoriteCustomer = name;
+
+changeLeastFavoriteCustomer();
